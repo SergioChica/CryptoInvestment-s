35ee8118957d1460da434784ba73dbcfd5cf5ef9
SergioChica <sergioanjaci@gmail.com>
2025-03-04 17:25:39 -0500

    feat(web.php):added the path to handle the historical data retrieval function

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('crypto-prices', [CryptoController::class, 'getCryptoPrices']);
+Route::get('/crypto-prices', [CryptoController::class, 'getCryptoPrices']);
+Route::get('/crypto-historical-data', [CryptoController::class, 'getHistoricalData']);
 
 Route::get('crypto', function () {
     return view('crypto');
