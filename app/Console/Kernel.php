81eae2328a44e3f7922fb73ed632ec52d759bb00
SergioChica <sergioanjaci@gmail.com>
2025-03-05 16:48:49 -0500

    feat(Kernel.php): We create a kernel file and in it we create a schedule which will be executed every 2 minutes.

---
+++ app/Console/Kernel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Jobs\FetchCryptoPricesJob; // Asegúrate de importar el Job
+
+class Kernel extends ConsoleKernel
+{
+    protected function schedule(Schedule $schedule): void
+    {
+        // We schedule the Job to run every 2 minutes
+        $schedule->job(new FetchCryptoPricesJob())->everyTwoMinutes();
+    }
+
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+    }
+}
